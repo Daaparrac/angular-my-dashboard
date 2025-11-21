db00b398c961224b466eb517f2adc478e7f46b4a
Daniel Parra <dparrap@summa-sci.com>
2025-11-21 14:48:39 -0500

    config dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
 
+ public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(r => r.path )
+    .filter(route => !route.path?.includes(':'));
 }
